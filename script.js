c5525990c3afc5a52cbabea47f72847fdf2e97cd
Daiki Ikeda <tetra.chan.da.z.1000@gmail.com>
2023-03-17 20:25:49 +0900

    失礼しました。以下が省略なしの完全なコードです。
    このコードを使用して、以前の問題が再発しないことを確認してください。

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const choices = document.getElementById("choices");
 const gameContainer = document.querySelector(".game-container");
 let skipAnimation = false;
 let waitingForClick = false;
-let currentChoices = [];
 
 gameContainer.addEventListener("click", () => {
   if (waitingForClick) {
@@ -18,67 +17,50 @@ async function animateText(element, text) {
   const lines = text.split("\n");
 
   for (const line of lines) {
-    const lineElement = createLineElement(line);
+    const lineElement = document.createElement("div");
+
+    for (const char of line) {
+      const charSpan = document.createElement("span");
+      charSpan.className = "char";
+      charSpan.textContent = char;
+      charSpan.style.opacity = "0";
+      lineElement.appendChild(charSpan);
+    }
 
     element.appendChild(lineElement);
-    await revealLine(lineElement);
+
+    for (const charSpan of lineElement.children) {
+      charSpan.style.opacity = "1";
+      if (!skipAnimation) {
+        await new Promise((resolve) => setTimeout(resolve, 50));
+      } else {
+        break;
+      }
+    }
 
     if (skipAnimation) {
-      showRemainingLines(lines, line);
+      element.innerHTML = "";
+      for (const remainingLine of lines.slice(lines.indexOf(line))) {
+        const remainingLineElement = document.createElement("div");
+        remainingLineElement.textContent = remainingLine;
+        element.appendChild(remainingLineElement);
+      }
+      skipAnimation = false;
       break;
     }
 
     if (lines.indexOf(line) < lines.length - 1) {
-      await waitForClick();
+      waitingForClick = true;
+      await new Promise((resolve) => {
+        const clickHandler = () => {
+          waitingForClick = false;
+          gameContainer.removeEventListener("click", clickHandler);
+          resolve();
+        };
+        gameContainer.addEventListener("click", clickHandler);
+      });
     }
   }
-}
-
-function createLineElement(line) {
-  const lineElement = document.createElement("div");
-
-  for (const char of line) {
-    const charSpan = document.createElement("span");
-    charSpan.className = "char";
-    charSpan.textContent = char;
-    charSpan.style.opacity = "0";
-    lineElement.appendChild(charSpan);
-  }
-
-  return lineElement;
-}
-
-async function revealLine(lineElement) {
-  for (const charSpan of lineElement.children) {
-    charSpan.style.opacity = "1";
-    if (!skipAnimation) {
-      await new Promise((resolve) => setTimeout(resolve, 50));
-    } else {
-      break;
-    }
-  }
-}
-
-function showRemainingLines(lines, currentLine) {
-  textBox.innerHTML = "";
-  for (const remainingLine of lines.slice(lines.indexOf(currentLine))) {
-    const remainingLineElement = document.createElement("div");
-    remainingLineElement.textContent = remainingLine;
-    textBox.appendChild(remainingLineElement);
-  }
-  skipAnimation = false;
-}
-
-async function waitForClick() {
-  waitingForClick = true;
-  await new Promise((resolve) => {
-    const clickHandler = () => {
-      waitingForClick = false;
-      gameContainer.removeEventListener("click", clickHandler);
-      resolve();
-    };
-    gameContainer.addEventListener("click", clickHandler);
-  });
 }
 
 function parseChoices(text) {
@@ -110,19 +92,19 @@ function showChoices(choiceList) {
 async function loadGame(file) {
   const response = await fetch(file);
   const text = await response.text();
-  currentChoices = parseChoices(text);
+  const choiceList = parseChoices(text);
   const content = text.replace(/choice:.+/g, "").replace(/->\s*(.+)/g, "").replace(/::[^:]+::/g, "").trim();
   textBox.innerHTML = "";
   skipAnimation = false;
   await animateText(textBox, content);
-  showChoices(currentChoices);
+  showChoices(choiceList);
 }
 
 function handleChoice(index) {
-  const chosen = currentChoices[index];
-  if (chosen.target) {
+  const choice = currentChoices[index];
+  if (choice.target) {
     choices.innerHTML = "";
-    loadGame(chosen.target + ".txt");
+    loadGame(choice.target + ".txt");
   }
 }
 
